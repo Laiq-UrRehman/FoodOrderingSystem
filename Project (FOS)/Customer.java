67356338d857c0db0aa44c11a0a8913fc13cf6e9
Laiq Ur Rehman <laiqmufti1019@gmail.com>
2026-05-07 15:26:13 +0500

    Add files via upload

---
+++ Project (FOS)/Customer.java
@@ -0,0 +1,6 @@
+public class Customer extends Person {
+    public Customer(){}
+    public Customer(String personID, String name, String address, String phoneNumber){
+        super(personID, name, address, phoneNumber);
+    }
+}
---
+++ Project (FOS)/Rider.java
@@ -0,0 +1,31 @@
+public class Rider extends Person {
+    private String vehicleType;
+    private boolean isAvailable;
+    private boolean isAssigned;
+
+    public Rider(){ }
+
+    public Rider(String personID, String name, String address, String phoneNumber, String vehicleType, boolean isAvailable){
+        super(personID, name, address, phoneNumber);
+        this.vehicleType = vehicleType;
+        this.isAvailable = isAvailable;
+        this.isAssigned = false;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+    public void assignOrder(){
+        if(isAvailable){
+            isAssigned = true;
+            return;
+        }
+        isAssigned = false;
+    }
+    public boolean getStatus(){
+       return isAvailable;
+    }
+}
